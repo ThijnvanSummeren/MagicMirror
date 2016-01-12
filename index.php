766be780766d88dde40f5c3a487040af75799bbc
root <thijnvansummeren@gmail.com>
2016-01-11 23:28:03 -0600

    config.js updated

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Magic Mirror</title>
+	<title> magicmirror </title>
 	<style type="text/css">
 		<?php include('css/main.css') ?>
 	</style>
@@ -28,7 +28,7 @@
 <script src="js/config.js"></script>
 <script src="js/rrule.js"></script>
 <script src="js/version/version.js" type="text/javascript"></script>
-<script src="js/calendar/calendar.js" type="text/javascript"></script>
+
 <script src="js/compliments/compliments.js" type="text/javascript"></script>
 <script src="js/weather/weather.js" type="text/javascript"></script>
 <script src="js/time/time.js" type="text/javascript"></script>
--- js/config.js
+++ js/config.js
@@ -1,36 +1,38 @@
 var config = {
     lang: 'nl',
     time: {
-        timeFormat: 12
+        timeFormat: 24
     },
     weather: {
         //change weather params here:
         //units: metric or imperial
         params: {
-            q: 'Baarn,Netherlands',
+            q: 'Eindhoven,Netherlands',
             units: 'metric',
             // if you want a different lang for the weather that what is set above, change it here
             lang: 'nl',
-            APPID: 'YOUR_FREE_OPENWEATHER_API_KEY'
+            APPID: '48230bb4484186c5de0410781058c8b4'
         }
     },
     compliments: {
         interval: 30000,
         fadeInterval: 4000,
         morning: [
-            'Good morning, handsome!',
-            'Enjoy your day!',
-            'How was your sleep?'
+            'Goede morgen!',
+            'Lekker geslapen?',
+            'Hoe is het?',
+            'Alles goed?'
         ],
         afternoon: [
-            'Hello, beauty!',
-            'You look sexy!',
-            'Looking good today!'
+            'Goede middag!',
+            'Hoe was je dag?',
+            'Hoe is het?',
+            'Alles goed?'
         ],
         evening: [
-            'Wow, you look hot!',
-            'You look nice!',
-            'Hi, sexy!'
+            'Goede avond!',
+            'Hoe is het?',
+            'Alles goed?'
         ]
     },
     calendar: {
